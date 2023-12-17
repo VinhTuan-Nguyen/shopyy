466eccdfb8fddaea831cb91623e78be6e9c583a7
Nguyen Vinh Tuan <vinhtuanbm@gmail.com>
2023-12-17 22:05:21 +0700

    update struceture

--- src/app/layouts/header/header-with-search/header-with-search.layout.ts
+++ src/app/layouts/header/header-with-search/header-with-search.layout.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { NavbarLink1_Element } from '../../../components/elements/navbar-link-1/navbar-link-1.element';
-import { NavbarLink2_Element } from '../../../components/elements/navbar-link-2/navbar-link-2.element';
-import { ShopyyLogo_Element } from '../../../components/elements/shopyy-logo/shopyy-logo.element';
-import { SearchBar_Element } from '../../../components/elements/searchbar/searchbar.element';
-import { HotDeals_Element } from '../../../components/elements/hot-deals/hot-deals.element';
+import { NavbarLink1_Element } from '../../../components/navbar-link-1/navbar-link-1.element';
+import { NavbarLink2_Element } from '../../../components/navbar-link-2/navbar-link-2.element';
+import { ShopyyLogo_Element } from '../../../components/shopyy-logo/shopyy-logo.element';
+import { SearchBar_Element } from '../../../components/searchbar/searchbar.element';
+import { HotDeals_Element } from '../../../components/hot-deals/hot-deals.element';
 
 @Component({
   selector: 'layout-header-with-search',
