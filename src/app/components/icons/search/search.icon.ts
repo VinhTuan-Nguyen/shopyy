e5ba445f6e0c9957d3d5613f030feb40deba8009
Nguyen Vinh Tuan <vinhtuanbm@gmail.com>
2023-12-18 11:29:55 +0700

    update header

---
+++ src/app/components/icons/search/search.icon.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'icon-search',
+    standalone: true,
+    imports: [],
+    templateUrl: './search.icon.html'
+})
+
+export class Search_Icon {}
--- src/app/components/searchbar/searchbar.component.ts
+++ src/app/components/searchbar/searchbar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { Search_Icon } from '../icons/search/search.icon';
 
 @Component({
     selector: 'component-searchbar',
     standalone: true,
-    imports: [],
+    imports: [
+        Search_Icon
+    ],
     templateUrl: './searchbar.component.html',
     styleUrl: './searchbar.component.css'
 })
