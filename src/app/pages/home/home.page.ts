01c86fedd84ee86beaf2650fab9d13a8ea4d1f3a
Nguyen Vinh Tuan <vinhtuanbm@gmail.com>
2023-12-25 16:24:51 +0700

    update home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { layout_HeaderWithSearch } from '../../layouts/header-with-search/header-with-search.layout';
 import { layout_HomeBanner } from '../../layouts/home-banner/home-banner.layout';
 import { layout_CategoryCarousel } from '../../layouts/category-carousel/category-carousel.layout';
+import { layout_TodaySuggestion } from '../../layouts/today-suggestion/today-suggestion.layout';
 
 @Component({
   selector: 'home-page',
@@ -9,7 +10,8 @@ import { layout_CategoryCarousel } from '../../layouts/category-carousel/categor
   imports: [
     layout_HeaderWithSearch,
     layout_HomeBanner,
-    layout_CategoryCarousel
+    layout_CategoryCarousel,
+    layout_TodaySuggestion
   ],
   templateUrl:  './home.page.html',
   styleUrl: './home.page.css'
